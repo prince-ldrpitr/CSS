af2e9d0d96f609092c29a2efb325ea7942351712
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-02-16 22:03:55 +0530

    ar

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int p;
+    int n;
     printf("enter the number is :");
-    scanf("%d",&p);
-    int root = sqrt(p);
+    scanf("%d",&n);
+    int root = sqrt(n);
     printf("the root of number is :%d",root);
     return 0;
 }
